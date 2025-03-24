ff5b9c642a19decbbd408ec6d77871211b1fa84e
JulioGaekel <juliogaekel@gmail.com>
2025-03-24 15:51:34 +0100

    Update contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,10 +6,17 @@ const Contact = () => {
                 action="https://formspree.io/f/your_form_id"
                 method="POST"
                 className="bg-white p-6 rounded-lg shadow-md w-full max-w-lg">
-                    <label className="block text-gray-700 font-medium">Your Name</label>
+                    <label className="block text-gray-400 font-medium">Your name</label>
                     <input type="text" name="name" className="w-full border p-2 rounded mt-1 mb-4" required />
 
-                </form>
+                    <label className="block text-gray-400 font-medium">Your email address</label>
+                    <input type="email" name="email" ClassName="w-full border p-2 rounded mt-1 mb-4" required />
+
+                    <label className="block text-gray-400 font-medium">Message</label>
+                    <textarea name="message" rows="5" className="w-full border p-2 rounded mt-1 mb-4" required></textarea>
+
+                    <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700">Send message</button>
+            </form>
         </section>
     );
 };
