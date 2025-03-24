4bf27bcd3ae1314faf1b84649cf0be9c887609cf
JulioGaekel <juliogaekel@gmail.com>
2025-03-24 17:16:12 +0100

    Add animations

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,24 @@
+import { motion } from "framer-motion";
+
 const Home = () => {
     return (
         <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-blue-500 to-purple-600 text-white">
-            <h1 className="text-5xl font-bold mb-4">Hi, I'm Julio</h1>
-            <p className="text-xl mb-6">I am a software developer passionate about building cool applications.</p>
+            <motion.h1
+            className="text-5xl font-bold mb-4"
+            initial={{ opacity: 0, y: -50}}
+            animate={{ opacity: 1, y: 0}}
+            transition={{ duration:1}}
+            >
+                Moin, I'm Julio
+            </motion.h1>
+            <motion.p
+                className="text-xl mb-6"
+                initial={{ opacity: 0 }}
+                animate={{opacity: 1 }}
+                transition={{ delay: 1, duration: 1}}
+            >
+                I am a software developer passionate about building cool applications.
+            </motion.p>
             <a
                 href="/projects"
                 className="px-6 py-3 bg-white text-blue-600 font-semibold rounded-lg shadow-md hover:bg-gray-200 transition"
