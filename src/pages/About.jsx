7855d403e4eebc74ef164ac3e9b6f3d8bbe4a35c
juliogaekel <juliogaekel@gmail.com>
2025-04-02 11:22:16 +0200

    Modify placeholder

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,8 @@
 const About = () => {
     return (
         <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-            <h1 className="text-4xl font-bold">About</h1>
-            <p className="mt-4 text-lg">PLACEHOLDER TEXT</p>
+            <h1 className="text-4xl font-bold">About Me</h1>
+            <p className="mt-4 px-36 text-lg">Hi there! Thanks for visiting my portfolio. I'm currently updating this section to better showcase my skills, experience, and journey as a developer. Stay tuned — more about me is coming soon, including the technologies I work with and the projects I'm most proud of.</p>
         </div>
     );
 };
