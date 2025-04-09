cdbaf595ee32437f0ac197792134f55523c8c939
juliogaekel <juliogaekel@gmail.com>
2025-04-09 16:25:37 +0200

    Add new image compressor project

--- src/projectsData.js
+++ src/projectsData.js
@@ -1,6 +1,14 @@
 const projects = [
     {
         id: 1,
+        title: "Image Compressor",
+        description: "Simple Python tool to compress images easily",
+        techStack: ["Python", "Tkinter"],
+        github: "https://github.com/JulioGaekel/image-compressor",
+        live: "PLACEHOLDER.COM",
+    },
+    {
+        id: 2,
         title: "Task Manager App",
         description: "A web app to track daily tasks and productivity.",
         techStack: ["React", "Node.js", "MongoDB"],
@@ -8,7 +16,7 @@ const projects = [
         live: "PLACEHOLDER.COM",
     },
     {
-        id: 2,
+        id: 3,
         title: "Calculator",
         description: "PROJECT DESCRIPTION.",
         techStack: ["TECH1", "TECH2", "TECH3"],
@@ -16,16 +24,8 @@ const projects = [
         live: "PLACEHOLDER.COM",
     },
     {
-        id: 3,
-        title: "Web Scraper",
-        description: "PROJECT DESCRIPTION.",
-        techStack: ["TECH1", "TECH2", "TECH3"],
-        github: "https://github.com/juliogaekel/PROJECT-NAME",
-        live: "PLACEHOLDER.COM",
-    },
-    {
         id: 4,
-        title: "PROJECT NAME",
+        title: "Web Scraper",
         description: "PROJECT DESCRIPTION.",
         techStack: ["TECH1", "TECH2", "TECH3"],
         github: "https://github.com/juliogaekel/PROJECT-NAME",
