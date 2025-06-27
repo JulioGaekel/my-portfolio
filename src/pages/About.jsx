3099b826daadefd83ba09b767534074a88cb3abb
juliogaekel <juliogaekel@gmail.com>
2025-06-27 16:14:06 +0200

    Comments

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,6 +5,8 @@ const About = () => {
                 <h1 className="text-4xl font-bold justify-center">About Me</h1>
                 <p className="mt-4 px-36 text-lg">I am a software developer with a legal background, focused on web development and backend technologies. I bring experience in Python, SQL, React, and automation. I place great value on clean, maintainable code and clear documentation. I am seeking an entry-level position as a Junior Developer in an agile, collaborative team.</p>
             </div>
+            {/* BEWERBUNG SHOULD START HERE */}
+            
         </div>
     );
 };
